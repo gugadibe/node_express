164dd4e11f0f3251722e142e933b32a63d640a48
Gustavo Dibe <gugadibe@gmail.com>
2026-07-14 21:48:26 -0300

    ch03: created api using express

---
+++ ch03/meadowlark/meadowlark.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import expressHandlebars from 'express-handlebars'
+const app = express();
+const __dirname = import.meta.dirname;
+const port = process.env.PORT || 3000;
+
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple."
+];
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
+});
+
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
+});
+
+app.use((req, res, err, next) => {
+    console.error(err.message);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(port, () => {
+    console.log(`Express started on http://localhost:${port}; press Ctrl-C to terminate.`);
+});
